3d7edbf38e12f54318262b87c1843d04f1d598e1
Nikita Vasilev <thisish000me@gmail.com>
2022-10-21 16:54:22 +0300

    Initial commit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,8 @@
 fun main() {
-    println("First App!")
+    val amount = 100000
+    val minPercent = 35
+    var currentPercent = if ((amount/10000)*75 > minPercent) (amount/10000)*75 else minPercent
+
+    println("Ваша комиссия составляет $currentPercent р.")
+
 }
